85041f13cd95153425d4cec460af4418dbb1afca
Ashish Agrawal <ashish.agrawal65@gmail.com>
2025-05-25 13:41:41 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,69 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@heroui/link";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Navbar } from "@/components/navbar";
+import { HeartFilledIcon } from "@/components/icons";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen text-foreground bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen">
+            {/* <Navbar /> */}
+            <main className="container mx-auto max-w-7xl pt-10 px-6 flex-grow">
+              {children}
+            </main>
+            <footer className="w-full flex items-center justify-center py-3 tex-sm">
+              <Link
+                isExternal
+                className="flex items-center gap-1 text-xs"
+                href="https://linkedin.com/in/ashishjec1994/"
+                title="heroui.com homepage"
+              >
+                <span className="text-default-600">Made with</span>
+                <HeartFilledIcon size={20} color="red" />{" "}
+                <span className="text-default-600">by</span>
+                <p className="text-primary">Ashish</p>
+              </Link>
+            </footer>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,58 @@
+"user client";
+
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import { LinkedInIcon } from "@/components/icons";
+import { Alert } from "@heroui/alert";
+import { Image } from "@heroui/image";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center gap-20 px-6 py-8 md:px-28 md:pt-32">
+      <div className="grid gap-8 md:grid-cols-2 md:gap-4 w-full">
+        {/* Left Section (Text + Buttons) */}
+        <div className="order-2 md:order-1 space-y-6 md:pt-20 text-center md:text-left">
+          <h1 className="text-xl">
+            I'm Ashish, a Staff Software Engineer with 9+ years of experience
+            designing full-stack systems and driving engineering excellence.
+          </h1>
+
+          <div className="flex flex-wrap gap-3 justify-center md:justify-start">
+            <Link
+              isExternal
+              className={buttonStyles({ variant: "bordered", radius: "full" })}
+              href={siteConfig.links.github}
+            >
+              <GithubIcon size={20} />
+              GitHub
+            </Link>
+            <Link
+              isExternal
+              className={buttonStyles({ variant: "bordered", radius: "full" })}
+              href={siteConfig.links.linkedIn}
+            >
+              <LinkedInIcon size={20} />
+              LinkedIn
+            </Link>
+          </div>
+        </div>
+
+        {/* Right Section (Image) */}
+        <div className="order-1 md:order-2 mx-auto">
+          <Image
+            isBlurred
+            alt="Ashish Profile"
+            src="/profile_pic.jpeg"
+            width={280}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
